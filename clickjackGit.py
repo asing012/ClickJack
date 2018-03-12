cf3cf85dfc28a95897c5ce5f0933e6e31f41a41c
Akshay Singh <akshayc_118@hotmail.com>
2018-03-12 18:12:53 -0400

    Update clickjackGit.py
    Comments added

--- clickjackGit.py
+++ clickjackGit.py
@@ -2,18 +2,18 @@ __author__ = 'akshaysingh'
 
 import requests
 
-f = open('filename', 'r')
+f = open('filename', 'r') #Put your filename that has all the website links.
 x = f.read().splitlines()
 a = 1
 
 for i in x:
     try:
         r = requests.get(i)
-        foundClick = dict(r.headers).get('X-Frame-Options')
+        foundClick = dict(r.headers).get('X-Frame-Options') #Checks X-Frame-Options
    
         #print(foundClick)
 
-        if(str(foundClick) == 'None'):
+        if(str(foundClick) == 'None'): #If X-Frame-Option is set to none
             print(a,"FOUND CLICKJACK ", i)
             a = a + 1
         else:
